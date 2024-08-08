450b56a32c9d6a94292f6e5ae9797733ceb4fa79
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-08-09 00:25:39 +0200

    7-airport.js

--- 0x02-ES6_classes/5-main.js
+++ 0x02-ES6_classes/5-main.js
@@ -1,3 +1,4 @@
+// 5-main.js
 import Building from './5-building.js';
 
 const b = new Building(100);
@@ -6,8 +7,7 @@ console.log(b);
 class TestBuilding extends Building {}
 
 try {
-    new TestBuilding(200)
-}
-catch(err) {
+    new TestBuilding(200);
+} catch (err) {
     console.log(err);
 }
--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,19 +1,18 @@
-// 6-sky_high.js
 import Building from './5-building.js';
 
 export default class SkyHighBuilding extends Building {
-constructor(sqft, floors) {
-    super(sqft);  // Call the parent class constructor to initialize _sqft
+  constructor(sqft, floors) {
+    super(sqft); // Call the parent class constructor to initialize _sqft
     this._floors = floors;
-}
+  }
 
   // Getter for floors
-get floors() {
+  get floors() {
     return this._floors;
-}
+  }
 
   // Override evacuationWarningMessage method
-evacuationWarningMessage() {
+  evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors`;
-}
+  }
 }
