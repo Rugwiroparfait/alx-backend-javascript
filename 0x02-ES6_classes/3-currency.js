a1f2f14bdd8251cdc1edb8322eb9236122cb1685
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-08-09 00:07:25 +0200

    5-building.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,39 +1,29 @@
-// implements a class named Currency
-
 export default class Currency {
-    constructor(code, name) {
-      if (typeof (code) !== 'string') {
-        throw new TypeError('code must be a string');
-      } else if (typeof (name) !== 'string') {
-        throw new TypeError('name must be a string');
-      }
-      this._code = code;
-      this._name = name;
-    }
-  
-    get code() {
-      return this._code;
-    }
-  
-    set code(newCode) {
-      if (typeof (newCode) !== 'string') {
-        throw new TypeError('code must be a string');
-      }
-      this._code = newCode;
-    }
-  
-    get name() {
-      return this._name;
-    }
-  
-    set name(newName) {
-      if (typeof (newName) !== 'string') {
-        throw new TypeError('name must be a string');
-      }
-      this._name = newName;
-    }
-  
-    displayFullCurrency() {
-      return (`${this._name} (${this._code})`);
-    }
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
   }
+
+  // Getter and Setter for code
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  // Getter and Setter for name
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  // Method to display full currency
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,34 +1,36 @@
-// implements a class Pricing that extends Currency
+// 4-pricing.js
+import Currency from './3-currency.js';
 
-import Currency from './3-currency';
-
-export default class Pricing extends Currency {
+export default class Pricing {
   constructor(amount, currency) {
-    super(currency.code, currency.name);
     this._amount = amount;
     this._currency = currency;
   }
 
+  // Getter and Setter for amount
   get amount() {
     return this._amount;
   }
 
-  set amount(newAmount) {
-    this._amount = newAmount;
+  set amount(value) {
+    this._amount = value;
   }
 
+  // Getter and Setter for currency
   get currency() {
     return this._currency;
   }
 
-  set currency(newCurrency) {
-    this._currency = newCurrency;
+  set currency(value) {
+    this._currency = value;
   }
 
-  displayFullCurrency() {
-    return `${this._amount} ${this.currency.name} ${this.currency.code}`;
+  // Method to display full price
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
   }
 
+  // Static method to convert price
   static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }
